bfc1e9de3c2789888fba8b2c0c559f86b056045c
pkgl <patrick.koegel@ghgmedia.de>
2023-02-13 20:49:39 +0100

    Removed multiple data resources

--- main.tf
+++ main.tf
@@ -11,17 +11,27 @@ provider "spotify" {
   api_key = var.spotify_api_key
 }
 
+locals {
+  tracks = {
+      miss_you = "https://open.spotify.com/track/4tRhRLBxIZ34Iw0eCuiC03?si=6506cf637de74da0",
+      doja = "https://open.spotify.com/track/3LtpKP5abr2qqjunvjlX5i?si=813c0207d7f042c1",
+      holy_trinity = "https://open.spotify.com/track/02D7cBNL1rpCkvTja8S1ns?si=34fbb3b99bdd402c",
+      ferrari = "https://open.spotify.com/track/4zN21mbAuaD0WqtmaTZZeP?si=f8c19b2574ba4c22",
+      kein_schlaf = "https://open.spotify.com/track/6416zJN0FGPmh1Ph4BH2h3?si=33802419891a4b95",
+      gas = "https://open.spotify.com/track/3QsIonKmbe7lollXunUVRZ?si=793c96a9ab6143a2",
+      affen = "https://open.spotify.com/track/6QNgcN2uHhXJG4RWDPyQQF?si=3dddd6e146704d9d",
+      rumble = "https://open.spotify.com/track/1GfBLbAhZUWdseuDqhocmn?si=9784615a9cb4431f",
+  }
+}
+
 resource "spotify_playlist" "playlist" {
   name        = "Terraform Playlist"
   description = "This playlist was created by Terraform"
   public      = true
 
   tracks = [
-    data.spotify_track.miss_you.id,
-    data.spotify_track.doja.id,
-    data.spotify_track.holy_trinity.id,
-    data.spotify_track.ferrari.id,
-    data.spotify_track.kein_schlaf.id,
-    data.spotify_track.gas.id,
+    for track in data.spotify_track.tracks:
+      track.id
   ]
+
 }
